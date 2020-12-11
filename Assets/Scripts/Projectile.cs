e8729f189cda6891fc6170812f5d01539bd96f78
Adam <grzybojad@gmail.com>
2020-12-12 00:36:27 +0100

    Added a Projectile Shooter, Projectile and a simple showcase

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    public float speed = 500f;
+    public float spawnLifetime = 2f;
+    
+    Rigidbody2D rb;
+    Vector2 direction;
+    float remainingLifetime;
+
+    public void Init( Vector2 shootDirection )
+    {
+        direction = shootDirection;
+    }
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        remainingLifetime = spawnLifetime;
+    }
+
+    void Update()
+    {
+        remainingLifetime -= Time.deltaTime;
+
+        if( remainingLifetime < 0 )
+        {
+            Destroy( gameObject );
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce( direction * (speed * Time.fixedDeltaTime), ForceMode2D.Impulse );
+    }
+
+    void OnTriggerEnter( Collider other )
+    {
+        // TODO add triggers away from camera that destroy projectiles
+    }
+}
