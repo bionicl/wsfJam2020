dda4b3ee3d3f657c11472786258640bb14e1335d
Bionicl <maciejmaj@me.com>
2020-12-12 01:03:14 +0100

    Basic vinyl add/remove

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    UiManager ui;
+
+    [Header("Settings")]
+    public int maxVinyls = 3;
+    public float startLevel = .5f;
+    [Tooltip("Per second")]
+    public float levelDecreaseSpeed = .02f;
+
+    // Internal
+    int _vinylNum;
+    float level;
+
+    private void Awake() {
+        instance = this;
+        level = startLevel;
+    }
+    private void Start() {
+        ui = UiManager.instance;
+    }
+
+    private void Update() {
+        level -= levelDecreaseSpeed * Time.deltaTime;
+        ui.slider.value = level;
+    }
+
+    public bool AddVinyl() {
+        if (_vinylNum >= maxVinyls)
+            return false;
+        _vinylNum++;
+        ui.AddVinyl();
+        return true;
+    }
+
+    public bool RemoveVinyl() {
+        if (_vinylNum <= 0)
+            return false;
+        ui.RemoveVinyl();
+        _vinylNum--;
+        return false;
+    }
+
+    // DEBUG
+    public void AddVinylButton() {
+        AddVinyl();
+    }
+    public void RemoveVinylButton() {
+        RemoveVinyl();
+    }
+
+
+}
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager instance;
+
+    [Header("UI elements")]
+    public Slider slider;
+
+    [Header("Spawn areas")]
+    public Transform vinylSpawnArea;
+
+    [Header("Prefabs")]
+    public GameObject vinylPrefab;
+
+
+    // Internal
+    Stack<GameObject> vinyls = new Stack<GameObject>();
+ 
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public void AddVinyl() {
+        GameObject tempGo = Instantiate(vinylPrefab, vinylSpawnArea);
+        vinyls.Push(tempGo);
+    }
+
+    public void RemoveVinyl() {
+        Destroy(vinyls.Pop());
+    }
+}
