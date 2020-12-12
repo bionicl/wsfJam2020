c0b7c8b2a87f8b063e85bf90f8d556aee9c1b443
Adam <grzybojad@gmail.com>
2020-12-12 22:49:38 +0100

    Fixed ground check and jump

---
+++ Assets/Scripts/PlayerScripts/ProjectileShooter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace PlayerScripts
+{
+    public class ProjectileShooter : MonoBehaviour
+    {
+        public GameObject projectilePrefab;
+        public float maxShotsPerSecond;
+
+        float timeSinceLastShot;
+
+        void Update()
+        {
+            timeSinceLastShot += Time.deltaTime;
+        }
+
+        public void TryShootOnce( Vector2 direction )
+        {
+            if (timeSinceLastShot < 1 / maxShotsPerSecond)
+                return;
+
+            if (!GameManager.instance.RemoveVinyl())
+                return;
+
+            GameObject projectile = Instantiate( projectilePrefab, transform );
+            projectile.GetComponent<Projectile>().Init( direction );
+
+            timeSinceLastShot = 0;
+        }
+    }
+}
