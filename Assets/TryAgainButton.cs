185c17b298b55343c4838f2d3a002708c490fc1c
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-12-13 02:26:30 +0100

    try again button

---
+++ Assets/TryAgainButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TryAgainButton : MonoBehaviour
+{
+    private Button button;
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TryAgain);
+    }
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
